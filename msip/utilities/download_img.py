91c6178d3825ef0e1c2feaac77f504f8988c8a86
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-07-08 23:15:57 +0900

    画像ダウンロードスクリプトの追加

--- msip/utilities/download_img.py
+++ msip/utilities/download_img.py
@@ -1,17 +1,22 @@
-if exist('./data/','dir') == 7
-    fnames = {'lena' 'baboon' 'goldhill' 'barbara'};
-    for idx = 1:length(fnames)
-        fname = [ fnames{idx} '.png' ];
-        if exist(sprintf('./data/%s',fname),'file') ~= 2
-            img = imread(...
-                sprintf('http://homepages.cae.wisc.edu/~ece533/images/%s',...
-                fname));
-            imwrite(img,sprintf('./data/%s',fname));
-            fprintf('Downloaded and saved %s in ./data\n',fname);
-        else
-            fprintf('%s already exists in ./data\n',fname);
-        end
-    end
-else
-    fprintf('./data folder does not exist\n');
-end
+import os.path
+from PIL import Image
+from urllib.request import urlopen
+
+def run():
+    if os.path.isdir('./data/'):
+        fnames = [ 'lena', 'baboon', 'goldhill', 'barbara' ]
+        for idx in range(len(fnames)):
+            fname = fnames[idx] + '.png'
+            if not os.path.isfile('./data/{0}'.format(fname)):
+                url = 'http://homepages.cae.wisc.edu/~ece533/images/{0}'.format(fname)
+                img = Image.open(urlopen(url))
+                img.save('./data/{0}'.format(fname))
+                print('Downloaded and saved {0} in ./data'.format(fname))
+            else:
+                print('{0} already exists in ./data'.format(fname))
+    else:
+        print('./data folder does not exist')
+
+
+if __name__ == '__main__':
+    run()
